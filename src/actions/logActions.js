46468ae83e737761392e8d5ad3a69455aa4dabf1
Laryd <hillarymuboka@gmail.com>
2022-03-28 17:31:13 +0300

    first commit

---
+++ src/actions/logActions.js
@@ -0,0 +1,43 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+
+export const getLogs = () => {
+  //return async (dispatch) => {
+  //setLoading();
+
+  //const res = await fetch("/logs");
+  //const data = await res.json();
+
+  //dispatch({
+  // type: GET_LOGS,
+  //  payload: data,
+  // });
+  //};
+  //};
+
+  //get logs from server
+  return async (dispatch) => async (dispatch) => {
+    try {
+      setLoading();
+
+      const res = await fetch("/logs");
+      const data = await res.json();
+
+      dispatch({
+        type: GET_LOGS,
+        payload: data,
+      });
+    } catch (err) {
+      dispatch({
+        type: LOGS_ERROR,
+        payload: err.response.data,
+      });
+    }
+  };
+};
+
+//set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
