b86192ac92ff70ebd7bda59f6926fb9bce9f67af
Laryd <hillarymuboka@gmail.com>
2022-07-16 13:06:56 +0300

    prepare for deploy

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
+const path = require("path");
 const PORT = process.env.PORT || 5000;
-server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
+
 server.use(router);
+//serve static assets in production
+if (process.env.NODE_ENV == "production") {
+  //set static folder
+  server.use(middlewares);
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "build", "index.html"))
+  );
+}
 server.listen(PORT, () => {
   console.log("Server is running");
 });
